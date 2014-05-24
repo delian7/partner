6978fdfb506a36373820c84dbaf43934a53af7b0
cryder <ryderc@uci.edu>
2014-05-24 12:08:48 -0700

    isis change

--- db/migrate/20140522025214_devise_create_users.rb
+++ db/migrate/20140522025214_devise_create_users.rb
@@ -14,6 +14,7 @@ class DeviseCreateUsers < ActiveRecord::Migration
 
       ## Trackable
       t.integer  :sign_in_count, default: 0, null: false
+      t.string   :ucinetid
       t.datetime :current_sign_in_at
       t.datetime :last_sign_in_at
       t.string   :current_sign_in_ip
