e1f25c9bc44212adab4018ffae23a71d02c6e1d7
Delian Petrov <petrovd@uci.edu>
2014-10-15 16:44:35 -0700

    fixed the current_user not signed in error

--- app/controllers/visitors_controller.rb
+++ app/controllers/visitors_controller.rb
@@ -2,8 +2,10 @@ class VisitorsController < ApplicationController
   def index
   	  @users = User.all
   	  # @list = Roster.where(:user_id=>current_user.id)
-  	  @list = current_user.groups.all
-  	  #@list = Roster.where(:user_id=>current_user.id)
+  	  if user_signed_in?
+        @list = current_user.groups.all
+  	    #@list = Roster.where(:user_id=>current_user.id)
+      end
   	  # @enrolled = @enrolled.collect(&:id)
 	  #@user = User.find(params[:id])
 	  authorize @users
