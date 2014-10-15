eaa71f38f750e17e57f1d7674bac3e85f868d799
cryder <ryderc@uci.edu>
2014-10-15 16:30:31 -0700

    new

--- app/controllers/visitors_controller.rb
+++ app/controllers/visitors_controller.rb
@@ -1,6 +1,11 @@
 class VisitorsController < ApplicationController
   def index
   	  @users = User.all
+  	  # @list = Roster.where(:user_id=>current_user.id)
+  	  @list = current_user.groups.all
+  	  
+  	  #@list = Roster.where(:user_id=>current_user.id)
+  	  # @enrolled = @enrolled.collect(&:id)
 	  #@user = User.find(params[:id])
 	  authorize @users
   end
