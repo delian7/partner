6b29e32606a736d2149440dcf241295b6b7c2f58
Delian Petrov <petrovd@uci.edu>
2014-10-18 16:22:16 -0700

    added new project form

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -4,4 +4,28 @@ class ProjectsController < ApplicationController
     @projects = Project.all
   end
   
+  def show
+    @project = Project.find(params[:id])
+  end
+  
+  def new
+    @project = Project.new(:active => true)
+  end
+  
+  def create
+    @project = Project.new(project_params)
+    
+    if @project.save
+      redirect_to(:action => 'index')
+    else
+      render('new')
+    end
+  end
+  
+  
+  def project_params
+    params.require(:project).permit(:name, :active, :course_id)
+  end 
+  
 end
+
