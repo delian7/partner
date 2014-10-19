773ad6456919efb2406843ad6c58020b13128369
Delian Petrov <petrovd@uci.edu>
2014-10-19 16:42:08 -0700

    cleaned up routes file

--- config/routes.rb
+++ config/routes.rb
@@ -10,39 +10,46 @@ devise_for :users, :controllers => {:registrations => "registrations"}
   root :to => "visitors#index"
   
   resources :users do
-     member do
-        get :cancel
-        get :confirm
-        get :registrations
-        get :sessions
-        get :clearpartnership
-        get :add_to_group
-        get :ungroup
-        post :set_current_course
-        post :request
-        end
-     collection do
-       get :clearall
-       get :export
-        end
+    member do
+      get :cancel
+      get :confirm
+      get :registrations
+      get :sessions
+      get :clearpartnership
+      get :add_to_group
+      get :ungroup
+      post :set_current_course
+      post :request
     end
-
+    collection do
+     get :clearall
+     get :export
+    end
+  end
+    
   resources :courses do
-     member do
-        end
-     collection do
-        end
+   member do
+   end
+   collection do
+   end
+  end
+    
+  resources :groups do
+    member do
     end
+    collection do 
+    end
+  end
 
   resources :projects do
-     member do
-       get :edit
-       post :update
-       get :remove
-        end
-     collection do
-        end
+    member do
+      get :edit
+      post :update
+      get :remove
     end
+    collection do
+    end
+  end
 
 devise_scope :user do
     get "sign_in", to: "devise/sessions#new"
