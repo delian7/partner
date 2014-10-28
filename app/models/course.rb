9440b9eb866bfbf804805354da9d8ff224a179f4
Delian Petrov <petrovd@uci.edu>
2014-10-27 18:11:47 -0700

    conflict resolution

--- app/models/course.rb
+++ app/models/course.rb
@@ -3,10 +3,6 @@ class Course < ActiveRecord::Base
 	has_many :groups, :through => :group_relations
 	has_many :rosters
 	has_many :group_relations
-<<<<<<< HEAD
-	has_many :projects, :through => :group_relations
-=======
 	has_many :projects, :through => :group_relations, :source => :project
->>>>>>> adding-groups
-  
+
 end
--- app/models/group_relation.rb
+++ app/models/group_relation.rb
@@ -2,9 +2,5 @@ class GroupRelation < ActiveRecord::Base
 	belongs_to :group
 	belongs_to :user
 	belongs_to :course
-<<<<<<< HEAD
-	belongs_to :project
-=======
   belongs_to :project
->>>>>>> adding-groups
 end
