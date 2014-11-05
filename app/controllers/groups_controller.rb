6ef50ea2890e6a5649a16d9c712b2904e32e9c1c
Delian Petrov <petrovd@uci.edu>
2014-11-04 22:22:22 -0800

    groups fix

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -6,9 +6,13 @@ class GroupsController < ApplicationController
   def index
     @users = User.all
     @courses = Course.all
-    @groups = Group.all
     authorize User.all
-    @course = Course.find(current_user.current_course)
+    @course = Course.find_by_id(current_user.current_course)
+    
+    
+    current_project = Project.where(course_id: current_user.current_course, active: true).first
+    allowed_group_size = Project.find_by_id(current_project.id).group_size
+    current_group_size = GroupRelation.where(project_id: current_project.id, course_id: current_user.current_course).uniq.pluck(:user_id).size
     
     # there should be only one current project active at a time... TODO error checking!
     @course_projects = @course.projects
