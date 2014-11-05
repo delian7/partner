dd9fadb145032d14071d6c357cb4cf4aa726d5e2
Delian Petrov <petrovd@uci.edu>
2014-11-04 23:20:46 -0800

    setting current project

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -10,13 +10,15 @@ class GroupsController < ApplicationController
     @course = Course.find_by_id(current_user.current_course)
     
     
-    current_project = Project.where(course_id: current_user.current_course, active: true).first
-    allowed_group_size = Project.find_by_id(current_project.id).group_size
-    current_group_size = GroupRelation.where(project_id: current_project.id, course_id: current_user.current_course).uniq.pluck(:user_id).size
-    
+    @current_projects = Project.where(course_id: current_user.current_course, active: true)
+    # @allowed_group_size = Project.find_by_id(@current_project.id).group_size
+    # @current_group_size = GroupRelation.where(project_id: current_project.id, course_id: current_user.current_course).uniq.pluck(:user_id).size
+
     # there should be only one current project active at a time... TODO error checking!
     @course_projects = @course.projects
   end
+  
+
 
   private
   def secure_params
