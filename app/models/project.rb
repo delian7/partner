7a117162694f631b11581217fde43230aa7dab4f
cryder <ryderc@uci.edu>
2014-11-17 00:30:00 -0800

    interface changes
    icons, button fixes, ordering users by last name, ordering groups by
    name

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,7 +1,8 @@
 class Project < ActiveRecord::Base
-   has_many :groups
+   has_many :groups, :through => :group_relations
    has_many :evaluations
    belongs_to :user
+   has_many :users, :through => :group_relations
    has_many :group_relations
    belongs_to :group
    has_many :courses, :through => :group_relations, :source => :course
