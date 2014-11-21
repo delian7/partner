da2d9dd4be838b2748a5e77cf9dacc6d91cea34f
Delian Petrov <petrovd@uci.edu>
2014-11-20 19:59:47 -0800

    added new specs

--- spec/features/features_spec.rb
+++ spec/features/features_spec.rb
@@ -10,26 +10,36 @@ Warden.test_mode!
 feature 'navigation to project page' do
   scenario 'professor should be able to go to the project page' do
     user = FactoryGirl.create(:professor)
+    login_as(user, :scope => :user)
     visit(projects_path)
     expect(page).to have_content 'Add New Project'
   end  
 end
 
+
+
 feature 'roster upload should populate students' do
+  scenario 'professor uploads roster for INF191' do
+    user = FactoryGirl.create(:professor)
+    login_as(user, :scope => :user)
+    visit(roster_path)
+    file_path = Rails.root + "SQL\ and\ CSVs/exampleroster.csv"
+    attach_file('dump_file', file_path, visible: false)
+    click_button 'Upload CSV File'
+    visit(users_path)
+    expect(page).to have_content 'Sabina Both'
+    
+  end
+    
+    
+  
   scenario 'students from roster upload should be in the list of students' do
     user = FactoryGirl.create(:student)
     roster = FactoryGirl.create(:roster)
     login_as(user, :scope => :user)
     visit(users_path)
     find('#user_current_course')
-    
-    
-    
-    
-    
-    
-  
-    
+        
     expect(page).to have_content 'Selected course:'
     
   end
