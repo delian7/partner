ad0f1e8d680b22575392006b9a1ebec3488e2b7d
cryder <ryderc@uci.edu>
2014-12-11 12:44:20 -0800

    for new batch uploads

--- config/application.rb
+++ config/application.rb
@@ -5,6 +5,12 @@ require 'rails/all'
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
+if defined?(Bundler)
+  # If you precompile assets before deploying to production, use this line
+  #Bundler.require(*Rails.groups(:assets => %w(development test)))
+  # If you want your assets lazily compiled in production, use this line
+   Bundler.require(:default, :assets, Rails.env)
+end
 
 module PartnerUp
   class Application < Rails::Application
