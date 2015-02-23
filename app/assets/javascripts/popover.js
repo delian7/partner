7e424ddf542e187b8942c926d7d8b1b5b6ebd967
cryder <ryderc@uci.edu>
2015-02-23 09:17:01 -0800

    styles and javascript

--- app/assets/javascripts/popover.js
+++ app/assets/javascripts/popover.js
@@ -1,5 +1,33 @@
-$(function () {
-	$('.info-popover').popover({ 
-	    html : true
-	  })
+$(function() {
+    $('.info-popover').popover({
+        html: true
+    })
+});
+
+
+
+$(document).on('change', '.btn-file :file', function() {
+    var input = $(this),
+        numFiles = input.get(0).files ? input.get(0).files.length : 1,
+        label = input.val().replace(/\\/g, '/').replace(/.*\//, '');
+    input.trigger('fileselect', [numFiles, label]);
+    if ($('#fileupload').val() != "")
+        $('#csvbutton').attr('disabled', false);
+    else
+        $('#csvbuton').attr('disabled', true);
+
+});
+$(document).ready(function() {
+    $('.btn-file :file').on('fileselect', function(event, numFiles, label) {
+        var input = $(this).parents('.input-group').find(':text'),
+            log = numFiles > 1 ? numFiles + ' files selected' : label;
+        if (input.length) {
+            input.val(log);
+        } else {
+            if (log) alert(log);
+        }
+    });
+    $('#csvbutton').on('click', function() {
+        $(this).button('loading') // button text will be "finished!"
+    })
 });
