f6cc39709560501f8bc7ac914c8fbc828c190a9f
cryder <ryderc@uci.edu>
2015-03-07 12:22:07 -0800

    customfailure

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -241,8 +241,9 @@ Devise.setup do |config|
   #
   config.warden do |manager|
     manager.default_strategies(:scope => :user).unshift :local_override
+    manager.failure_app = CustomFailure
   end
-
+  
   # ==> Mountable engine configurations
   # When using Devise inside an engine, let's call it `MyEngine`, and this engine
   # is mountable, there are some extra configurations to be taken into account.
